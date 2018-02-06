df211657645e12a43bebbec4aadba02cbc17f8c8
Jerome <decje9@gmail.com>
2018-02-05 21:06:50 -0500

    Merge branch 'vectorized'

--- lib/tests/integration/log2.cpp
+++ lib/tests/integration/log2.cpp
@@ -4,26 +4,27 @@
 #include <cmath>
 
 
-//TEST_CASE("Log2 ASM")
-//{
-//    using namespace std::chrono_literals;
-//
-//    auto buffer = std::stringstream();
-//    auto assembly_input = std::ifstream("lib/tests/integration/log2.laval", std::ios::in | std::ios::out);
-//    Assembler::preprocess(assembly_input, buffer);
-//    auto [ast, settings] = Assembler::parse(buffer);
-//
-//    auto output = std::stringstream();
-//
-//    Assembler::assemble(ast, settings, output);
-//
-//    auto cpu = Assembler::load_binary(output);
-//
-//    for (auto a = 5_u8; a < 0xffu; a++)
-//    {
-//        auto test = cpu.start(0s, std::vector({a}));
-//        auto answer = static_cast<int>(test);
-//        INFO(a);
-//        REQUIRE(answer == std::floor(std::log2(a)));
-//    }
-//}
+TEST_CASE("Log2 ASM")
+{
+    using namespace std::chrono_literals;
+
+    auto buffer = std::stringstream();
+    auto assembly_input = std::ifstream("lib/tests/integration/log2.laval", std::ios::in | std::ios::binary);
+    Assembler::preprocess(assembly_input, buffer);
+    auto [ast, settings] = Assembler::parse(buffer);
+
+    auto output = std::stringstream();
+    output.seekg(0);
+
+    Assembler::assemble(ast, settings, output);
+
+    auto cpu = Assembler::load_binary(output);
+
+    for (auto a = 5_u8; a < 0xff_u8; a++)
+    {
+        std::istringstream input("0," + std::to_string(a));
+        auto answer = static_cast<int>(cpu.start(input, std::cout));
+        INFO(a);
+        REQUIRE(answer == std::floor(std::log2(a)));
+    }
+}
